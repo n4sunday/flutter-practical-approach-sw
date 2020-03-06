740511bc73c1b4f9d9a5f854e0a70220a40b361b
Sunday <s5935512004@phuket.psu.ac.th>
2020-03-06 14:36:22 +0700

    :bulb: List view ui

--- lib/main.dart
+++ lib/main.dart
@@ -2,132 +2,102 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+var headerSection = Image.network(
+    "https://www.computing.psu.ac.th/th/wp-content/uploads/2017/12/Campus-Life.png",
+    height: 250);
+var titleSection = Container(
+    padding: EdgeInsets.all(20),
+    color: Colors.white30,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Expanded(
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "PSU Phuket",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Phuket, Thailand",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+              ]),
+        ),
+        Column(
+          children: <Widget>[Icon(Icons.thumb_down)],
+        ),
+        Column(
+          children: <Widget>[
+            Text(
+              "99",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            )
+          ],
+        )
+      ],
+    ));
+var buttonSection = Container(
+  margin: EdgeInsets.only(top: 20),
+  color: Colors.white,
+  height: 90,
+  child:
+      Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: <Widget>[
+    Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          Icons.thumb_up,
+          size: 40,
+        ),
+        Text("Like", style: TextStyle(fontSize: 24))
+      ],
+    ),
+    Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Icon(Icons.comment, size: 40),
+        Text("Comment", style: TextStyle(fontSize: 24))
+      ],
+    ),
+    Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Icon(Icons.share, size: 40),
+        Text("Share", style: TextStyle(fontSize: 24))
+      ],
+    )
+  ]),
+);
+var courseSection = Container(
+  color: Colors.white,
+  height: 200,
+  child: ListView(scrollDirection: Axis.horizontal, children: <Widget>[
+    Row(children: <Widget>[
+      Image.asset('assets/images/admission.jpg'),
+      Image.asset('assets/images/domitory.jpg'),
+      Image.asset('assets/images/service.jpg'),
+      Image.asset('assets/images/campus.jpg'),
+    ])
+  ]),
+);
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "Test Icon",
+        title: "UI",
         home: Scaffold(
-            backgroundColor: Colors.yellow,
-            appBar: AppBar(title: Text('Icon Widget')),
-            body: Container(
-              color: Colors.white,
-              child: Row(children: <Widget>[
-                Icon(Icons.beenhere, size: 50, color: Colors.amber),
-                Icon(Icons.bluetooth, size: 50, color: Colors.green),
-                Icon(Icons.cloud_upload, size: 50, color: Colors.red)
-              ]),
-            )));
+          appBar: AppBar(title: Text("Computer Enginering")),
+          body: ListView(
+            children: <Widget>[
+              headerSection,
+              titleSection,
+              buttonSection,
+              courseSection,
+            ],
+          ),
+        ));
   }
 }
-
-// class BigBlueBox extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//       width: 100,
-//       height: 200,
-//       decoration:
-//           BoxDecoration(color: Colors.indigo[200], border: Border.all()),
-//     );
-//   }runAppckgroundColor: Colors.greenAccent,
-//           body: Column(
-//             children: <Widget>[
-//               Row(
-//                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                 children: [
-//                   Image.asset(
-//                     "assets/images/The-Witcher-3-Wild-Hunt-Review-6.jpg",
-//                     height: 250,
-//                   ),
-//                   Column(
-//                     children: <Widget>[
-//                       Text(
-//                         'Nattapon',
-//                         style: TextStyle(
-//                           color: Colors.black,
-//                           fontWeight: FontWeight.w800,
-//                           fontFamily: 'Roboto',
-//                           letterSpacing: 0.5,
-//                           fontSize: 20,
-//                         ),
-//                       ),
-//                       Text(
-//                         'Lueakaew',
-//                         style: TextStyle(
-//                           color: Colors.black,
-//                           fontWeight: FontWeight.w800,
-//                           fontFamily: 'Roboto',
-//                           letterSpacing: 0.5,
-//                           fontSize: 20,
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                 ],
-//               ),
-//             ],
-//           ),
-//         ));
-//   }
-// // }
-
-// import 'package:flutter/material.dart';
-// import 'package:english_words/english_words.dart';
-
-// void main() => runApp(MyApp());
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     final wordPair = WordPair.random();
-//     return MaterialApp(
-//       title: 'Test StatefulWidget',
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: Text('STATEFULWIDGET'),
-//         ),
-//         body: Center(
-//           child: Text('Word: $wordPair '),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// }
-
-// import 'package:flutter/material.dart';
-// import 'package:english_words/english_words.dart';
-
-// void main() => runApp(MyApp());
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Test StatefulWidget',
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: Text('STATEFULWIDGET'),
-//         ),
-//         body: Center(
-//           child: RandomWords(),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// class RandomWords extends StatefulWidget {
-//   @override
-//   RandomWordsState createState() => RandomWordsState();
-// }
-
-// class RandomWordsState extends State<RandomWords> {
-//   @override
-//   Widget build(BuildContext context) {
-//     final wordPair = WordPair.random();
-//     return Text('Word: $wordPair ');
-//   }
-// }
